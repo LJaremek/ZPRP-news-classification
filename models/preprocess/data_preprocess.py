abd91c3217878d9f092cdd772936979a1091b6ef
Patrycja Wysocka <w512patrycja@gmail.com>
2024-01-08 16:42:42 +0100

    black reformatted data_preprocess file

--- models/preprocess/data_preprocess.py
+++ models/preprocess/data_preprocess.py
@@ -6,16 +6,26 @@ from collections import Counter
 
 # Changes in DataFrame
 
+
 def tokenize(data: pd.DataFrame, text_column: str) -> None:
-    data['tokens'] = data.apply(lambda row: word_tokenize(row[text_column].lower()), axis=1)
+    """
+    adds extra columns with tokens
+    """
+    data["tokens"] = data.apply(
+        lambda row: word_tokenize(row[text_column].lower()), axis=1
+    )
 
 
 def label_change(data: pd.DataFrame, label_column: str) -> pd.DataFrame:
-    data = data.replace({label_column: {'real': 1, 'fake': 0}})
+    """
+    changes label column from str representation to int
+    """
+    data = data.replace({label_column: {"real": 1, "fake": 0}})
     return data
 
 
 # Tokenization
+
 
 def create_corpus(token_column: pd.Series) -> Counter:
     """
@@ -34,7 +44,7 @@ def corpus_to_int(corpus: Counter, token_column) -> list[int]:
     """
     Changes text represantation to numbers
     """
-    word_to_int = {word: n+1 for n, (word, count) in enumerate(corpus)}
+    word_to_int = {word: n + 1 for n, (word, count) in enumerate(corpus)}
     text_int = []
     for text in token_column:
         article = [word_to_int[word] for word in text]
@@ -42,24 +52,24 @@ def corpus_to_int(corpus: Counter, token_column) -> list[int]:
     return text_int
 
 
-def pad_tokens(text_int, seq_len):
+def pad_tokens(text_int: list[int], seq_len: int) -> np.array:
     """
     Pads tokens to given value of sequence length
     """
-    ready_articles = np.zeros((len(text_int), seq_len), dtype = int)
+    ready_articles = np.zeros((len(text_int), seq_len), dtype=int)
 
     for n, article in enumerate(text_int):
         if len(article) <= seq_len:
             zeros = list(np.zeros(seq_len - len(article)))
             new = zeros + article
         else:
-            new = article[: seq_len]
+            new = article[:seq_len]
         ready_articles[n, :] = np.array(new)
 
     return ready_articles
 
 
-def prepare_data(token_column, seq_len):
+def prepare_data(token_column: pd.Series, seq_len: int) -> np.array:
     """
     returns array of ints of every article from dataframe
     """
