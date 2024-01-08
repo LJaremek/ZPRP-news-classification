6bb9ca3e0d5b44fb5bf8457bedff4da0ae10952e
Patrycja Wysocka <w512patrycja@gmail.com>
2024-01-08 16:34:29 +0100

    added converting text to int and pad_tokens funcitons

--- models/preprocess/data_preprocess.py
+++ models/preprocess/data_preprocess.py
@@ -1,11 +1,59 @@
 import pandas as pd
+import numpy as np
 from nltk.tokenize import word_tokenize
+from collections import Counter
 
 
-def tokenize(data: pd.DataFrame, text_col: str) -> None:
-    data['tokens'] = data.apply(lambda row: word_tokenize(row[text_col].lower()), axis=1)
+# Changes in DataFrame
+
+def tokenize(data: pd.DataFrame, text_column: str) -> None:
+    data['tokens'] = data.apply(lambda row: word_tokenize(row[text_column].lower()), axis=1)
 
 
-def label_change(data: pd.DataFrame, label_col: str) -> pd.DataFrame:
-    data = data.replace({label_col: {'real': 1, 'fake': 0}})
+def label_change(data: pd.DataFrame, label_column: str) -> pd.DataFrame:
+    data = data.replace({label_column: {'real': 1, 'fake': 0}})
     return data
+
+
+# Tokenization
+
+def create_corpus(token_column: pd.Series) -> Counter:
+    """
+    Create vocabulary of words from all texts in dataframe
+    with its frequency of occurence
+    """
+    corpus = []
+    for text in token_column:
+        corpus.extend(text)
+    count_words = Counter(corpus)
+
+    return count_words.most_common()
+
+
+def corpus_to_int(corpus: Counter, token_column) -> list[int]:
+    """
+    Changes text represantation to numbers
+    """
+    word_to_int = {word: n+1 for n, (word, count) in enumerate(corpus)}
+    text_int = []
+    for text in token_column:
+        article = [word_to_int[word] for word in text]
+        text_int.append(article)
+    return text_int
+
+
+def pad_tokens(text_int, seq_len):
+    """
+    Pads tokens to given value of sequence length
+    """
+    ready_articles = np.zeros((len(text_int), seq_len), dtype = int)
+
+    for n, article in enumerate(text_int):
+        if len(article) <= seq_len:
+            zeros = list(np.zeros(seq_len - len(article)))
+            new = zeros + article
+        else:
+            new = article[: seq_len]
+        ready_articles[n, :] = np.array(new)
+
+    return ready_articles
