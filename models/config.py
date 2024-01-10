7ed8a5fcbd5cdb506d209094ba87748f38249dc1
Patrycja Wysocka <w512patrycja@gmail.com>
2024-01-10 17:18:38 +0100

    correct config values to work with our saved model

--- models/config.py
+++ models/config.py
@@ -1,4 +1,6 @@
 # for preprocess
+PATH_TO_DATA = '/home/wpartycja/7sem/zrpr/zprp-projekt-z4/data/fake_news.csv'
+SEQ_LEN = 200
 TEST_SIZE = 0.2
 VAL_SIZE = 0.2
 RANDOM_STATE = 42
@@ -6,8 +8,9 @@ RANDOM_STATE = 42
 # for LSTM
 NUM_CLASSES = 2
 
+BATCH_SIZE = 32
 EMBEDDING_DIM = 50
-HIDDEN_DIM = 50
+HIDDEN_DIM = 200
 
 LSTM_LAYERS = 1
 IS_BIDIRECTIONAL = False
